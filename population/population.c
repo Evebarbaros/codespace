d1ec8a25fe5a6b27280d43e874b1748a6e7dca36
Evebarbaros <148644423+Evebarbaros@users.noreply.github.com>
2023-10-28 08:38:25 +0000

    Sat, Oct 28, 2023, 8:38 AM +00:00

--- population/population.c
+++ population/population.c
@@ -4,10 +4,22 @@
 int main(void)
 {
     // TODO: Prompt for start size
+int ni;
+do
+{
+    ni = get_int("add starting number : ");
+}
+while (ni > 9)
 
     // TODO: Prompt for end size
+int ns;
+do
+{
+    ns = get_int("add ending number : ");
+}
+while (ns > ni)
 
     // TODO: Calculate number of years until we reach threshold
-
+n 
     // TODO: Print number of years
 }
