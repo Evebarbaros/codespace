8bff46e59e2f943303714b2654dce49e51b6e44d
Evebarbaros <148644423+Evebarbaros@users.noreply.github.com>
2023-10-30 03:27:26 +0000

    Mon, Oct 30, 2023, 3:27 AM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -11,9 +11,9 @@ int main(void)
     while ( n < 1 ) ;
 
     {
-    for ( int i = 0 ; j < n ; j++)
+    for ( int i = 0 ; i < n ; i++)
         {
-            printf("  ")
+            printf("a")
         }
 
     }
